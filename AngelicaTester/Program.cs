ccb2c7710880e3bd6f0a14de0c079db81e0468de
Angelicapluf <angelicapluf@gmail.com>
2024-04-24 20:07:13 +1200

    Create new material record

--- AngelicaTester/Program.cs
+++ AngelicaTester/Program.cs
@@ -1,15 +1,18 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V120.Page;
 using System;
+using System.Threading;
 
 namespace AngelicaTester
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
             // open chrome  browser
-            IWebDriver  driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             // launch TurnUp portal
             driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
@@ -17,11 +20,11 @@ namespace AngelicaTester
             // identify username textbox and valid username
             IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
             usernameTextbox.SendKeys("hari");
-            
+
             // identify password textbox and enter valid password
             IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
             passwordTextbox.SendKeys("123123");
-            
+
             // click on login button
             IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
             loginButton.Click();
@@ -29,6 +32,81 @@ namespace AngelicaTester
             // check if user is logged in successfully
             IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
 
-        }
+            if (helloHari.Text == "Hello hari")
+            {
+                Console.WriteLine("Logged in sucessfuly, teest passed.");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, test failed");
+            }
+
+            // Create Time and Material record
+
+            // Go to TM page
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            // Click on Create New Button
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            createNewButton.Click();
+
+            //Select Material from TypeCode dropdown
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[2]/span"));
+            typeCodeDropdown.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement materialOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[1]"));
+            materialOption.Click();
+            
+            // Identify the code textbox and input a code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("AngelicaTester");
+
+            // Identify the description textbox and input a description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("AngelicaTester");
+
+            //Identify the price textbox and input price
+            IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            priceTag.Click();
+
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("12");
+
+            // Click on save button
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+
+            // Click on go to last page button
+            IWebElement goToLastButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            goToLastButton.Click();
+            Thread.Sleep(1000);
+
+            // Check if record is present in the table and has expected value
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+           
+            if (actualCode.Text == "AngelicaTester")
+            {
+                Console.WriteLine("Material record created successffully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Teste failed.");
+             }
+         }
     }
 }
+
+
+      
+
+
+
+
+
